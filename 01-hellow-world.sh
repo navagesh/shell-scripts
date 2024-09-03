65760f70127f5c2a1058626bac474fbadc4b4c02
navagesh <navagesh@gmail.com>
2024-09-03 20:06:18 +0530

    first file of shell-scripts format

---
+++ 01-hellow-world.sh
@@ -0,0 +1,15 @@
+i am learning shell-script
+
+Programing--> java, .net, python..
+Scripts --> shell, python, ansible, terraform, docker and kubernets ..etc
+
+Coding patterns and mothods of any scropts same
+-----------------------------------------------
+
+1. variables
+2. data types
+3. counditions
+4. functions
+5. loops
+
+algorithm thinking is very important
