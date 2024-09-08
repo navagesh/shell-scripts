088f423265591df946ab7dc325c2c375485bbc95
navagesh <deekondanagesh@gmail.com>
2024-09-08 15:38:33 +0530

    variable basic script

---
+++ 02-variable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#variable script
+
+PERSON1=Nagesh
+PERSON2=Vasu
+
+echo "$PERSON2: hi $PERSON1, good morning"
+echo "$PERSON1: hi $PERSON2, very good morning"
+echo "$PERSON1: how are you $PERSON2"
+echo "$PERSON2: ya iam good, how are you $PERSON1"
